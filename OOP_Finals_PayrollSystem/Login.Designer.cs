1c655ed4261c62e62531291898bdcaf816e3e205
Justin Paul Andarae Andalleon <202410201@feucavite.edu.ph>
2026-05-06 14:47:23 +0800

    rename form1

--- OOP_Finals_PayrollSystem/Login.Designer.cs
+++ OOP_Finals_PayrollSystem/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace OOP_Finals_PayrollSystem
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.txtUserName = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
--- OOP_Finals_PayrollSystem/Login.cs
+++ OOP_Finals_PayrollSystem/Login.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace OOP_Finals_PayrollSystem
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
