527b83b1b9ff913fa9bec1030e814b47d59559c7
Justin Paul Andarae Andalleon <202410201@feucavite.edu.ph>
2026-05-07 17:52:21 +0800

    Registration from

--- OOP_Finals_PayrollSystem/Regsitration.Designer.cs
+++ OOP_Finals_PayrollSystem/Regsitration.Designer.cs
@@ -69,12 +69,12 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 122);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(239, 25);
+            this.label1.Size = new System.Drawing.Size(267, 29);
             this.label1.TabIndex = 0;
-            this.label1.Text = "REGISTER NEW USER";
+            this.label1.Text = "Employee Information";
             // 
             // label2
             // 
