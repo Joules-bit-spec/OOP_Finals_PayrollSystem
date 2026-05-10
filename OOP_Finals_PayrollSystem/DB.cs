662bdd26b54e94b8a4803341551626f7c637d3c9
Joel Nicor <presjoel5@gmail.com>
2026-05-10 21:57:03 +0800

    MAJOR MAJOR CHANGE

---
+++ OOP_Finals_PayrollSystem/DB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Finals_PayrollSystem
+{
+    public static class DB
+    {
+        public static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HP\Documents\PayrollSystem.mdf;Integrated Security=True;Connect Timeout=30";
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+
+        public static int SaveEmployee(Employee employee)
+        {
+            using (SqlConnection connection = GetConnection())
+            {
+                string query = "INSERT INTO Employees (FirstName, LastName, MiddleInitial, Gender, Birthday, Email, ContactNumber, Address, Department, Position) VALUES (@FirstName, @LastName, @MiddleInitial, @Gender, @Birthday, @Email, @ContactNumber, @Address, @Department, @Position)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@FirstName", employee.GetFirstName());
+                    command.Parameters.AddWithValue("@LastName", employee.GetLastName());
+                    command.Parameters.AddWithValue("@MiddleInitial", employee.GetMiddleInitial());
+                    command.Parameters.AddWithValue("@Gender", employee.GetGender());
+                    command.Parameters.AddWithValue("@Birthday", employee.GetBirthday());
+                    command.Parameters.AddWithValue("@Email", employee.GetEmail());
+                    command.Parameters.AddWithValue("@ContactNumber", employee.GetContactNumber());
+                    command.Parameters.AddWithValue("@Address", employee.GetAddress());
+                    command.Parameters.AddWithValue("@Department", employee.GetDepartment());
+                    command.Parameters.AddWithValue("@Position", employee.GetPosition());
+
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+                    connection.Close();
+
+                    return rowsAffected;
+                }
+            }
+        }
+    }
+}
---
+++ OOP_Finals_PayrollSystem/Employee.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Finals_PayrollSystem
+{
+    public class Employee
+    {
+        private int EmployeeID;
+        private string FirstName;
+        private string LastName;
+        private string MiddleInitial;
+        private string Gender;
+        private string Birthday;
+        private string Email;
+        private string ContactNumber;
+        private string Address;
+        private string Department;
+        private string Position;
+
+        private int UserID;
+        private string Password;
+
+        public int GetEmployeeID()
+        {
+            return EmployeeID;
+        }
+
+        public string GetFirstName()
+        {
+            return FirstName;
+        }
+
+        public string GetLastName()
+        {
+            return LastName;
+        }
+
+        public string GetMiddleInitial()
+        {
+            return MiddleInitial;
+        }
+
+        public string GetFullName()
+        {
+            return $"{FirstName} {MiddleInitial}. {LastName}";
+        }
+
+        public string GetGender()
+        {
+            return Gender;
+        }
+
+        public string GetBirthday()
+        {
+            return Birthday;
+        }
+
+        public string GetEmail()
+        {
+            return Email;
+        }
+
+        public string GetContactNumber()
+        {
+            return ContactNumber;
+        }
+
+        public string GetAddress()
+        {
+            return Address;
+        }
+
+        public string GetDepartment()
+        {
+            return Department;
+        }
+
+        public string GetPosition()
+        {
+            return Position;
+        }
+
+        public int GetUserID()
+        {
+            return UserID;
+        }
+
+        public string GetPassword()
+        {
+            return Password;
+        }
+    }
+}
